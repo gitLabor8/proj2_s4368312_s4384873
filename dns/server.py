41c3579eca0c6a943becaa1aaa1a452f1cd2d632
Frank Gerlings <f.gerlings@student.science.ru.nl>
2016-05-19 17:22:28 +0200

    Handler: simple, untested

--- dns/server.py
+++ dns/server.py
@@ -4,8 +4,11 @@
 
 This module provides a recursive DNS server. You will have to implement this
 server using the algorithm described in section 4.3.2 of RFC 1034.
+
+See RFC 1034 section 3.6 for more information
 """
 
+from socket import AF_INET, SOCK_STREAM
 import socket
 from threading import Thread
 
@@ -13,15 +16,24 @@ from threading import Thread
 class RequestHandler(Thread):
     """ A handler for requests to the DNS server """
 
-    def __init__(self):
+    def __init__(self, clientSocket):
         """ Initialize the handler thread """
         super().__init__()
         self.daemon = True
+        self.clientSocket = clientSocket
         
     def run(self):
         """ Run the handler thread """
-        # TODO: Handle DNS request
-        pass
+        # Handle DNS request
+        caching = False
+        ttl = 10
+        resolver = Resolver(caching, ttl)
+        messageReceived = self.clientSocket.recv(1024)
+        hostname = message 	# for now, TODO Parse input
+        ip = resolver.gethostbyname(hostname)
+        messageSend = ip 	# TODO Create nice message
+        self.clientSocket.send(messageSend)
+        self.clientSocket.close()
 
 
 class Server(object):
@@ -38,16 +50,19 @@ class Server(object):
         self.caching = caching
         self.ttl = ttl
         self.port = port
-        # TODO: create socket
-
+        self.done = False
+        # Create socket
+        self.webSocket = socket.socket(AF_INET, SOCK_STREAM)
+        self.webSocket.bind(('', self.port))
     def serve(self):
         """ Start serving request """
-        # TODO: start listening
+        # Start listening
+        self.webSocket.listen(1)
         while not self.done:
-            # TODO: receive request and open handler
-            pass
-
+            # Receive request and open handler
+            (reqSocket, address) = self.webSocket.accept()
     def shutdown(self):
         """ Shutdown the server """
         self.done = True
-        # TODO: shutdown socket
+        # shutdown socket
+		self.webSocket.close()
